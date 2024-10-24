cfae66584c101b78d60a81b5498314b5976af5ac
Viktoria Zsuppan <viktori.zsu@gmail.com>
2024-10-24 15:58:26 +0200

    feladat megoldasa: ternalis operator

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -4,7 +4,7 @@ public class JavaGit {
 
     public static void main(String[] args) {
         /*ha hideg van akkor viszek kabatot egyebkent nem viszek kabatot*/
-        int homerseklet = 9;
+        int homerseklet = 11;
         boolean hideg = homerseklet < 10;
         String kabat = hideg ? "viszek kabatot" : "nem viszek kabatot";
         System.out.println(kabat);
