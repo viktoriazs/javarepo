7dd49e9a40d03a81ea8c262fc7ab687bc010ac5f
Viktoria Zsuppan <viktori.zsu@gmail.com>
2024-10-24 15:49:58 +0200

    Javagit.java: feladat leiras

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -3,6 +3,7 @@ package javagit;
 public class JavaGit {
 
     public static void main(String[] args) {
+        /*ha hideg van akkor viszek kabatot egyebkent nem viszek kabatot*/
         
     }
     
